821884f6b22c80b19ccc60d50ab02760bb8119f8
yuanyueshui <yuanyueshui@wepiao.com>
2017-08-10 15:40:54 +0800

    test

--- function/param.c
+++ function/param.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
+#include <stdarg.h> //可变参数头文件
 
 void change(int *num);
+//可变参数函数
+double avg(int num, ...);
 
 int main()
 {
     int num = 9;
     change(&num);
-    printf("%d", num);
+    printf("%d\n", num);
 
+    printf("%.2lf", avg(4, 2,3,4,5));
     return 0;
 }
 
 void change(int *num)
 {
     (*num)++;
+}
+
+double avg(int num, ...)
+{
+    va_list vaList;
+    double sum = 0.0;
+    /* 为 num 个参数初始化 valist */
+    va_start(vaList, num);
+    /* 访问所有赋给 valist 的参数 */
+    for (int i = 0; i < num; ++i) {
+        sum += va_arg(vaList, int);
+    }
+    /* 清理为 valist 保留的内存 */
+    va_end(vaList);
+
+    return sum/num;
 }
---
+++ test/test/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  test
+//
+//  Created by yuanyueshui on 2017/8/9.
+//  Copyright © 2017年 yuanyueshui. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    int aa;
+    aa = 1;
+   
+    printf("%d",aa);
+     aa = 2;
+     printf("%d",aa);
+    printf("Hello, World!\n");
+    return 0;
+}
